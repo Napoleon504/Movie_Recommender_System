4a04858fcdbd3b4bd9262b7b1d8a6ed1d935f608
Napoleon504 <49624647+Napoleon504@users.noreply.github.com>
2023-03-04 16:54:19 +0900

    Update OfflineRecommender.scala

--- recommender/OfflineRecommender/src/main/scala/LMF_based_CF/OfflineRecommender.scala
+++ recommender/OfflineRecommender/src/main/scala/LMF_based_CF/OfflineRecommender.scala
@@ -5,22 +5,22 @@ import org.apache.spark.mllib.recommendation.{ALS, Rating}
 import org.apache.spark.sql.SparkSession
 import org.jblas.DoubleMatrix
 
-// 基于评分数据的LFM，只需要rating数据
+// LFM based on rating data, only rating data is required
 case class MovieRating(uid: Int, mid: Int, score: Double, timestamp: Int)
 
 case class MongoConfig(uri:String, db:String)
 
-// 定义一个基准推荐对象
+// Define a Recommendation case class
 case class Recommendation(mid: Int, score: Double)
 
-// 定义基于预测评分的用户推荐列表
+// Define List of user recommendations based on predicted ratings
 case class UserRecs(uid: Int, recs: Seq[Recommendation])
 
-// 定义基于LMF电影特征向量的电影相似度列表
+// Define List of movie similarities based on LMF movie feature vectors
 case class MovieRecs(mid: Int, recs: Seq[Recommendation])
 
 object OfflineRecommender {
-  // 定义表名
+  // Define table names
   val MONGODB_RATING_COLLECTION = "Rating"
 
   val USER_RECS = "UserRecs"
@@ -33,45 +33,45 @@ object OfflineRecommender {
       "mongo.uri" -> "mongodb://localhost:27017/recommender",
       "mongo.db" -> "recommender"
     )
-    // 创建一个sparkConf
+    // Create a sparkConf
     val sparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("OfflineRecommender")
 
-    // 创建一个SparkSession
+    // create a SparkSession
     val spark = SparkSession.builder().config(sparkConf).getOrCreate()
     import spark.implicits._
 
     implicit val mongoConfig = MongoConfig(config("mongo.uri"), config("mongo.db"))
 
-    // 从mongodb加载数据
+    // Load data from mongodb
     val ratingRDD = spark.read
       .option("uri", mongoConfig.uri)
       .option("collection", MONGODB_RATING_COLLECTION)
       .format("com.mongodb.spark.sql")
       .load()
-      .as[MovieRating] // 转成case class data set
+      .as[MovieRating] // Transform to case class data set
       .rdd
-      .map(rating => (rating.uid, rating.mid, rating.score)) // 转换成rdd，并且去掉时间戳
+      .map(rating => (rating.uid, rating.mid, rating.score)) // Transform to rdd, remove timestamps
       .cache()
 
-    // 从rating中提取所有uid和mid，并去重
+    // Extract uid and mid from rating, and deduplicate
     val userRDD = ratingRDD.map(_._1).distinct()
     val movieRDD = ratingRDD.map(_._2).distinct()
 
-    // 训练隐语义模型
+    // Train LMF model
     val trainData = ratingRDD.map(x => Rating(x._1, x._2, x._3))
 
     val (rank, iterations, lambda) = (50, 5, 0.01)
     val model = ALS.train(trainData, rank, iterations, lambda)
 
-    // 基于用户和电影的隐特征，计算预测评分，得到用户推荐列表
-    // 计算user和movie的笛卡尔积，得到一个空评分矩阵
+    // Based on the hidden features of users and movies, calculate the predicted scores and get the user recommendation list
+    // Calculate the Cartesian product of user and movie to get an empty rating matrix
     val userMovies = userRDD.cartesian(movieRDD)
 
-    // 调用model的predict方法预测评分
+    // Call the predict method of the model to predict the score
     val preRatings = model.predict(userMovies)
 
     val userRecs = preRatings
-      .filter(_.rating > 0) // 过滤出评分大于0的项
+      .filter(_.rating > 0) // Filter out items with a score greater than 0
       .map(rating => (rating.user, (rating.product, rating.rating)))
       .groupByKey()
       .map{
@@ -86,15 +86,15 @@ object OfflineRecommender {
       .format("com.mongodb.spark.sql")
       .save()
 
-    // 基于电影特征，计算相似度矩阵，得到电影相似度列表
+    // Based on the movie features, calculate the similarity matrix and get the movie similarity list
     val movieFeatures = model.productFeatures.map{
       case (mid, features) => (mid, new DoubleMatrix(features))
     }
 
-    // 对所有电影两两计算它们的相似度，先做笛卡尔积
+    // Calculate the similarity of all movies pairwise, first do the Cartesian product
     val movieRecs = movieFeatures.cartesian(movieFeatures)
       .filter{
-        // 把与自己配对的过滤掉
+        // Filter out those that match itself
         case (a, b) => a._1 != b._1
       }
       .map{
@@ -103,7 +103,7 @@ object OfflineRecommender {
           (a._1, (b._1, simScore))
         }
       }
-      .filter(_._2._2 > 0.6) // 过滤出相似度大于0.6的
+      .filter(_._2._2 > 0.6) // Filter out those with a similarity greater than 0.6
       .groupByKey()
       .map{
         case (mid, items) => MovieRecs(mid, items.toList.sortWith(_._2 > _._2).map(x => Recommendation(x._1, x._2)))
@@ -120,7 +120,7 @@ object OfflineRecommender {
     spark.stop()
   }
 
-  // 求向量余弦相似度
+  // Calculate vector cosine similarity
   def consinSim(movie1: DoubleMatrix, movie2: DoubleMatrix): Double = {
     movie1.dot(movie2) / (movie1.norm2() * movie2.norm2())
   }
