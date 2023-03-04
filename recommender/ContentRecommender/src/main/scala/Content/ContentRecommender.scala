b1becd5c008a7dc6ffc8a427cccbd6719b51b201
Napoleon504 <49624647+Napoleon504@users.noreply.github.com>
2023-03-04 16:28:59 +0900

    Update ContentRecommender.scala

--- recommender/ContentRecommender/src/main/scala/Content/ContentRecommender.scala
+++ recommender/ContentRecommender/src/main/scala/Content/ContentRecommender.scala
@@ -6,20 +6,20 @@ import org.apache.spark.ml.linalg.SparseVector
 import org.apache.spark.sql.SparkSession
 import org.jblas.DoubleMatrix
 
-// 需要的数据源是电影内容信息
+// The required data source is movie content information
 case class Movie(mid: Int, name: String, descri: String, timelong: String, issue: String,
                  shoot: String, language: String, genres: String, actors: String, directors: String)
 
 case class MongoConfig(uri:String, db:String)
 
-// 定义一个基准推荐对象
+// Define a standard recommendation case class
 case class Recommendation( mid: Int, score: Double )
 
-// 定义电影内容信息提取出的特征向量的电影相似度列表
+// Define the movie similarity list of the feature vectors extracted from the movie content information
 case class MovieRecs( mid: Int, recs: Seq[Recommendation] )
 
 object ContentRecommender {
-  // 定义表名和常量
+  // Define table names and constants
   val MONGODB_MOVIE_COLLECTION = "Movie"
   val CONTENT_MOVIE_RECS = "ContentMovieRecs"
 
@@ -29,16 +29,16 @@ object ContentRecommender {
       "mongo.uri" -> "mongodb://localhost:27017/recommender",
       "mongo.db" -> "recommender"
     )
-    // 创建一个sparkConf
+    // Create a sparkConf
     val sparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("OfflineRecommender")
 
-    // 创建一个SparkSession
+    // Create a SparkSession
     val spark = SparkSession.builder().config(sparkConf).getOrCreate()
     import spark.implicits._
 
     implicit val mongoConfig = MongoConfig(config("mongo.uri"), config("mongo.db"))
 
-    // 加载数据，并做处理
+    // Load data and preprocess
     val movieTagsDF = spark.read
       .option("uri", mongoConfig.uri)
       .option("collection", MONGODB_MOVIE_COLLECTION)
@@ -46,30 +46,30 @@ object ContentRecommender {
       .load()
       .as[Movie]
       .map(
-        // 提取mid, name, genres三项作为原始内容特征，分词器默认按空格做分词
+        // extract mid, name, genres as features, the tokenizer defaults to splitting words by spaces
         x => (x.mid, x.name, x.genres.map(c => if(c == '|') ' ' else c))
       )
       .toDF("mid", "name", "genres")
       .cache()
 
-    // 核心部分：用TF-IDF从内容信息中提取电影特征向量
-    // 创建一个分词器，默认按空格分词
+    // Core part: Use TF-IDF to extract movie feature vectors from content information
+    // Create a tokenizer, the default is to split words by spaces
     val tokenizer = new Tokenizer().setInputCol("genres").setOutputCol("words")
 
-    // 用分词器对原始数据做转换，生成新的一列words
+    // Convert the original data with a tokenizer to generate a new list of words
     val wordsData = tokenizer.transform(movieTagsDF)
     // wordsData.show()
 
-    // 引入HashingTF工具，可以把一个词语序列转化成对应的词频
+    // Use HashingTF tool, which can convert a word sequence into the corresponding word frequency
     val hashingTF = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(50)
     val featurizedData = hashingTF.transform(wordsData)
     // featurizedData.show(truncate = false)
 
-    // 引入IDF工具，可以得到idf模型
+    // Use IDF tool to get the idf model
     val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-    // 训练idf模型，得到每个词的逆文档频率
+    // Train the idf model to get the inverse document frequency of each word
     val idfModel = idf.fit(featurizedData)
-    // 用模型对原数据进行处理，得到文档中每个词的tf-idf值，作为新的特征向量
+    // Use the model to deal with the original data to get the tf-idf value of each word in the document as a new feature vector
     val rescaledData = idfModel.transform(featurizedData)
     // rescaledData.show(truncate = false)
 
@@ -82,10 +82,10 @@ object ContentRecommender {
       )
     // movieFeatures.collect().foreach(println
 
-    // 对所有电影两两计算它们的相似度，先做笛卡尔积
+    // Calculate the similarity of all movies pairwise, first do the Cartesian product
     val movieRecs = movieFeatures.cartesian(movieFeatures)
       .filter {
-        // 把自己跟自己的配对过滤掉
+        // Filter out matches between the movie and itself
         case (a, b) => a._1 != b._1
       }
       .map {
@@ -94,7 +94,7 @@ object ContentRecommender {
           (a._1, (b._1, simScore))
         }
       }
-      .filter(_._2._2 > 0.6) // 过滤出相似度大于0.6的
+      .filter(_._2._2 > 0.6) // Filter out those with a similarity greater than 0.6
       .groupByKey()
       .map {
         case (mid, items) => MovieRecs(mid, items.toList.sortWith(_._2 > _._2).map(x => Recommendation(x._1, x._2)))
@@ -111,7 +111,7 @@ object ContentRecommender {
 
   }
 
-  // 求向量余弦相似度
+  // Calculate vector cosine similarity
   def consinSim(movie1: DoubleMatrix, movie2: DoubleMatrix): Double = {
     movie1.dot(movie2) / (movie1.norm2() * movie2.norm2())
   }
