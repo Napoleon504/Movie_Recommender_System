49b96661aff69093f66f38452e0477c995908db3
Napoleon504 <49624647+Napoleon504@users.noreply.github.com>
2023-03-04 16:47:47 +0900

    Update ALSTrainer.scala

--- recommender/OfflineRecommender/src/main/scala/LMF_based_CF/ALSTrainer.scala
+++ recommender/OfflineRecommender/src/main/scala/LMF_based_CF/ALSTrainer.scala
@@ -15,32 +15,32 @@ object ALSTrainer {
       "mongo.db" -> "recommender"
     )
 
-    // 创建一个sparkConf
+    // Create a sparkConf
     val sparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("ALSTrainer")
 
-    // 创建一个SparkSession
+    // Create a SparkSession
     val spark = SparkSession.builder().config(sparkConf).getOrCreate()
     import spark.implicits._
 
     implicit val mongoConfig = MongoConfig(config("mongo.uri"), config("mongo.db"))
 
-    // 从mongodb加载评分数据
+    // Load rating data from mongodb
     val ratingRDD = spark.read
       .option("uri", mongoConfig.uri)
       .option("collection", MONGODB_RATING_COLLECTION)
       .format("com.mongodb.spark.sql")
       .load()
-      .as[MovieRating] // 转成case class data set
+      .as[MovieRating] // Transform to case class data set
       .rdd
-      .map(rating => Rating(rating.uid, rating.mid, rating.score)) // 转换成rdd，并且去掉时间戳
+      .map(rating => Rating(rating.uid, rating.mid, rating.score)) // Transform to rdd, remove timestamps
       .cache()
 
-    // 随机切分数据集，生成训练集和数据集
+    // Randomly split the data set to generate training set and data set
     val splits = ratingRDD.randomSplit(Array(0.8, 0.2))
     val trainingRDD = splits(0)
     val testRDD = splits(1)
 
-    // 模型参数选择，输出最优参数
+    // Model parameter selection, output optimal parameters
     adjustALSParam(trainingRDD, testRDD)
 
     spark.close()
@@ -50,23 +50,23 @@ object ALSTrainer {
     val result = for(rank <- Array(20, 50 ,100); lambda <- Array(0.001, 0.01, 0.1))
       yield{
         val model = ALS.train(trainData, rank, 5, lambda)
-        // 计算当前参数对应模型的rmse，返回double
+        // Calculate the rmse of the model with current parameters and return double
         val rmse = getRMSE(model, testData)
         (rank, lambda, rmse)
       }
-    // 控制台打印最优参数
+    // Print the optimal parameters to console
     println(result.minBy(_._3))
   }
 
   def getRMSE(model: MatrixFactorizationModel, data: RDD[Rating]) = {
-    // 计算预测评分
+    // Compute predicted scores
     val userProducts = data.map(item => (item.user, item.product))
     val predictRating = model.predict(userProducts)
 
-    // 以uid，mid作为外键，inner join实际观测值和预测值
+    // Use uid and mid as foreign keys, inner join the actual observed value and predicted value
     val observed = data.map(item => ((item.user, item.product), item.rating))
     val predict = predictRating.map(item => ((item.user, item.product), item.rating))
-    // 内连接得到(uid, mid), (actual, predict)
+    // inner join to get (uid, mid), (actual, predict)
     sqrt(
       observed.join(predict).map {
         case ((uid, mid), (actual, pred)) =>
